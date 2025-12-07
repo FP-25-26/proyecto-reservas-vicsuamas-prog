e2cd61e4a6d63086352f5e1a73e50f52c0f20769
vicsuamas-prog <vicsuamas@alum.us.es>
2025-12-07 21:25:34 +0100

    Finalizado con bugs en 1, 4,7

--- src/reservas.py
+++ src/reservas.py
@@ -1,6 +1,6 @@
 from typing import NamedTuple
 from datetime import datetime,date
-
+import calendar
 import csv
 
 Reserva = NamedTuple("Reserva", 
@@ -30,7 +30,7 @@ def lee_reservas(ruta_fichero: str) -> list[Reserva]:
 
         for nombre,dni,fecha_entrada,fecha_salida,tipo_habitacion,num_personas,precio_noche,servicios_adicionales in lector:
 
-            fecha_entrada, fecha_salida= datetime.strptime(fecha_entrada,"%Y-%m-%d"),datetime.strptime(fecha_salida,"%Y-%m-%d")
+            fecha_entrada, fecha_salida= datetime.strptime(fecha_entrada,"%Y-%m-%d").date(),datetime.strptime(fecha_salida,"%Y-%m-%d").date()
             num_personas,precio_noche = int(num_personas),float(precio_noche)
 
             if bool(servicios_adicionales):
@@ -75,3 +75,111 @@ def total_facturado(reservas: list[Reserva], fecha_ini: date | None = None, fech
     #Se introduce bien en el bloque if, toma correctamente los parametros opcionales, la resta de fechas es correcta, obtiene correctamente los dias con .days, 
     #ningun error a la hora de multiplicar por el precio por noche, ningun error  al acceder a la lista de Reservas...
     #MODO DEBUG = el generador devuelve None ¿por que?
+
+def reservas_mas_largas(reservas: list[Reserva], n: int = 3) -> list[tuple[str, date]]:
+    '''
+    Devuelve las n reservas mas largas con el nombre del husped y la su fecha de entrada
+    
+    :param reservas: lista de tuplas de tipo Reserva
+    :type reservas: list[Reserva]
+    :param n: número de reservas a mostrar después del filtrado
+    :type n: int
+    :return: lista con las n reservas deseadas 
+    :rtype: list[tuple[str, date]]
+    '''
+
+    reservas_plus = sorted([(reserva.nombre,reserva.fecha_entrada,reserva.fecha_salida - reserva.fecha_entrada) for reserva in reservas] ,key = lambda x: x[2],reverse = True)
+    return [(x[0],x[1]) for x in reservas_plus][:n]
+
+def cliente_mayor_facturacion(reservas: list[Reserva],servicios: set[str] | None = None) -> tuple[str, float]:
+    '''
+    Devuelve el cliente con más facturación en el hotel con un filtro de servicios y esa facturación
+    
+    :param reservas: lista de tuplas de tipo Reserva
+    :type reservas: list[Reserva]
+    :param servicios: conjunto de servicios a filtrar
+    :type servicios: set[str] | None
+    :return: Description
+    :rtype: tuple[str, float]
+    '''
+    res = [(dni,sum(reserva.precio_noche * (reserva.fecha_salida - reserva.fecha_entrada).days for reserva in reservas if reserva.dni == dni )) 
+               for dni in {reserva.dni for reserva in reservas}]
+    
+    if servicios != None:
+        hss = [(dni,sum(reserva.precio_noche * (reserva.fecha_salida - reserva.fecha_entrada).days for reserva in reservas if reserva.dni == dni ),reserva.servicios_adicionales) 
+               for dni in {reserva.dni for reserva in reservas} for reserva in reservas]
+        
+        res = [(cliente[0],cliente[1]) for cliente in hss if bool(servicios &  set(cliente[2]))]
+        
+    return max(res,key=lambda x : x[1]) #Error en el Test
+
+def servicios_estrella_por_mes(reservas: list[Reserva], tipos_habitacion: set[str] | None = None) -> dict[str, str]:
+
+    '''
+    Devuelve un diccionario con el servicio estrella del mes de todos los tipos de habitaciones o unos tipos concretos
+    
+    :param reservas: lista de tuplas de tipo Reserva
+    :type reservas: list[Reserva]
+    :param tipos_habitacion: tipos de habitaciones a filtrar
+    :type tipos_habitacion: set[str] | None
+    :return: diccionario con los servicios estrella de cada mes
+    :rtype: dict[str, str]
+    '''
+
+    n_meses = set(range(1,13))
+    meses = ["", "enero", "febrero", "marzo", "abril", "mayo", "junio","julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+
+    servicios_mes = [(meses[mes],[servicio for reserva in reservas if reserva.fecha_entrada.month == mes 
+                                  for servicio in reserva.servicios_adicionales]) for mes in n_meses]
+    
+    if tipos_habitacion != None:
+
+        servicios_mes = [(meses[mes],[servicio for reserva in reservas if reserva.fecha_entrada.month == mes and reserva.tipo_habitacion in tipos_habitacion
+                                  for servicio in reserva.servicios_adicionales]) for mes in n_meses]
+        
+    res = {mes[0] : max(mes[1],key = mes[1].count) for mes in servicios_mes}
+        
+    return res
+
+
+def media_dias_entre_reservas(reservas: list[Reserva]) -> float:
+    '''
+    Devuelve la media de dias entre cada dos reservas (¿tasa de ocupación?)
+    
+    :param reservas: lista de tuplas de tipo Reserva
+    :type reservas: list[Reserva]
+    :return: media
+    :rtype: float
+    '''
+    reservas_ordenadas = sorted(reservas, key = lambda x : x.fecha_entrada)
+    medias= [(reservas_ordenadas[i+1].fecha_entrada - reservas_ordenadas[i].fecha_entrada).days for i in range(len(reservas_ordenadas) -1)]
+
+    return sum(medias) / len(medias)
+
+def cliente_reservas_mas_seguidas(reservas: list[Reserva], min_reservas: int) -> str:
+    '''
+    Devuelve el DNI y la media de días entre reservas consecutivas del cliente con al menos `min_reservas` y menor media de días entre reservas.
+
+    :param reservas: lista de tuplas de tipo Reserva
+    :type reservas: list[Reserva]
+    :param min_reservas: min de reservas para el cliente
+    :type min_reservas: int
+    :return: cadena con la media, el minimo de reservas y la menor media entre reservas
+    :rtype: str
+    '''
+ 
+    reservas_por_cliente = [(cliente, sum(1 for reserva in reservas if reserva.dni == cliente ),[reserva for reserva in reservas if reserva.dni == cliente]) 
+                            for cliente in {reserva.dni for reserva in reservas}] # (cliente, num reservas , dichas reservas)
+    
+    filtro_min = [elemento for elemento in reservas_por_cliente if min_reservas <= elemento[1]] #filtro de numero de reservas
+
+    reservas_ordenadas_por_cliente = [(elemento[0],(sorted(elemento[2],key = lambda x : x.fecha_entrada))) for elemento in filtro_min] #reservas ordenadas por fecha
+
+    medias_por_cliente = [(elemento[0],[(elemento[1][i+1].fecha_entrada - elemento[1][i].fecha_salida).days for i in range(len(elemento[1])-1)]) #listas de medias entre dos reservas consecutivas
+                          for elemento in reservas_ordenadas_por_cliente]
+    
+    cliente_elegido = min(((elemento[0],sum(elemento[1])/len(elemento[1])) for elemento in medias_por_cliente), key = lambda x : x[1]) #min de la media de las medias
+
+
+    return f"El DNI del cliente con al menos {min_reservas} reservas y menor media de días entre reservas consecutivas es  \
+    {cliente_elegido[0]}, con una media de días entre reservas de {cliente_elegido[1]}."
--- src/reservas_test.py
+++ src/reservas_test.py
@@ -5,7 +5,12 @@ def main():
     ruta = "./data/reservas.csv"
     datos = lee_reservas(ruta)
     #test_lee_reservas(ruta)
-    test_total_facturado(datos)
+    #test_total_facturado(datos)
+    #test_reservas_mas_largas(datos)
+    #test_cliente_mayor_facturacion(datos)
+    #test_servicios_estrella_por_mes(datos)
+    #test_media_dias_entre_reservas(datos)
+    print(cliente_reservas_mas_seguidas(datos,5))
 
 def test_lee_reservas(ruta):
     datos = lee_reservas(ruta)
@@ -22,5 +27,25 @@ def test_total_facturado(datos):
     print("Desde 1 de febrero de 2022 (fecha final None):", total_facturado(datos,fecha_ini,None))
     print("Hasta 28 de febrero de 2022 (fecha inicio None):",total_facturado(datos,None,fecha_ini))
 
+def test_reservas_mas_largas(datos):
+    print("TEST de reservas_mas_largas")
+    print("Con n=3 : ",reservas_mas_largas(datos))
+
+def test_cliente_mayor_facturacion(datos):
+    print("TEST de cliente_mayor_facturacion")
+    print("Sin flitrar por servicios: ", cliente_mayor_facturacion(datos))
+    print("Con parking: ",cliente_mayor_facturacion(datos,{"Parking",}))
+    print("Con parking o spa : ",cliente_mayor_facturacion(datos,{"Parking","Spa"}))
+
+def test_servicios_estrella_por_mes(datos):
+    print("TEST de servicio_estrella_por_mes")
+    print("Todos los tipos de habitación: ",servicios_estrella_por_mes(datos))
+    print("Habitación familiar o deluxe: ",servicios_estrella_por_mes(datos,{"Deluxe","Familiar"}))
+
+def test_media_dias_entre_reservas(datos):
+    print("TEST de media_dias_entre_reservas",media_dias_entre_reservas(datos))
+
+
 if __name__ == '__main__':
     main()
+    
